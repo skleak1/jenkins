95a5db177a0d53705c5b572af70401fc9a13a1a5
skleak1 <skingleak@gmail.com>
2026-03-05 15:37:08 +0700

    added more api endpoints

--- NodeJS-API/index.js
+++ NodeJS-API/index.js
@@ -11,6 +11,18 @@ app.get('/search', (req, res) => {
   res.send('Inside Searching API');
 });
 
+app.post('/add', (req, res) => {
+  res.send('Inside Adding API');
+});
+
+app.put('/update', (req, res) => {
+  res.send('Inside Updating API');
+});
+
+app.delete('/delete', (req, res) => {
+  res.send('Inside Deleting API');
+});
+
 app.listen(port, () => {
   console.log(`Express server started at http://localhost:${port}`);
 });
